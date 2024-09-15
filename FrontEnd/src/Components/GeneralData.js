5cec34c530070e4357bcec3f8dd7f55499f2fbea
Srikanth <mrmadukkur1804@gmail.com>
2024-09-15 23:38:25 +0530

    .

--- FrontEnd/src/Components/GeneralData.js
+++ FrontEnd/src/Components/GeneralData.js
@@ -1,3 +1,3 @@
-export let API_EndPoint = "https://foodapp-tjmu.onrender.com";
-//https://foodapp-tjmu.onrender.com
+export let API_EndPoint = "https://foodapp-qyr6.onrender.com";
+//https://foodapp-qyr6.onrender.com
 //http://localhost:8000
